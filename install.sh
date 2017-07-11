ffa2692e991a320de8509423d87a2a14ae7fe29c
Travis Clarke <travis.m.clarke@gmail.com>
2017-07-10 18:56:42 -0700

    add install.sh setup script

---
+++ install.sh
@@ -0,0 +1,65 @@
+#!/bin/sh
+
+OS=$(uname -s)
+ARCH=$(uname -m)
+
+BIN_DIR="/usr/local/bin"
+SCRIPT='jwt'
+
+DARWIN='darwin'
+LINUX='linux'
+X86_64='x86_64'
+
+BUILD_DIR=
+
+# Check operating system
+case "$OS" in
+  [Dd]arwin*)
+    BUILD_DIR="$DARWIN"
+  ;;
+  [Ll]inux*)
+    BUILD_DIR="$LINUX"
+  ;;
+  *)
+    echo "unknown operating system: $OS" >&2
+    exit 1
+  ;;
+esac
+echo "$OS operating system detected"
+
+# Check machine architecture
+case "$ARCH" in
+  [Xx]86*|[Xx]86[-_]64*)
+    BUILD_DIR="$BUILD_DIR-$X86_64"
+  ;;
+  *)
+    echo "unknown machine architecture: $ARCH" >&2
+    exit 1
+  ;;
+esac
+echo "$ARCH machine architecture detected"
+
+# Make directory (if doesn't exist)
+if [ ! -d "$BIN_DIR" ]; then
+  echo "creating directory $BIN_DIR"
+  sudo mkdir -p "$BIN_DIR"
+fi
+
+copy_script () {
+  echo "copying $BUILD_DIR/$SCRIPT to $BIN_DIR"
+  sudo cp -f "$BUILD_DIR/$SCRIPT" "$BIN_DIR"
+}
+
+# Copy script
+if ! type "$SCRIPT" > /dev/null 2>&1; then
+  copy_script
+else
+  read -r -n1 -p "$SCRIPT already exists on your PATH. Do you still want to install? [N/y] "
+  echo
+  if [ "$REPLY" == 'y' ] || [ "$REPLY" == 'Y' ]; then
+    copy_script
+  else
+    echo "setup aborted" >&2
+    exit 1
+  fi
+fi
